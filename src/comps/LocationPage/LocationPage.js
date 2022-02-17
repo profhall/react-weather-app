cab3f5b78424de01e41a55e43945d555f6a53ba2
Professor Hall <iprofhall@gmail.com>
2022-02-16 22:32:29 -0500

    added bg photo to header

--- src/comps/LocationPage/LocationPage.js
+++ src/comps/LocationPage/LocationPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect,useRef } from 'react';
-import { Container, Row, Collapse,Col, Button, Navbar, ListGroup } from 'react-bootstrap';
+import { Container, Row, Collapse,Col, Navbar, ListGroup } from 'react-bootstrap';
 import TestIcon from  "../../images/sun.png"
 
 const daysOfWeek ={
@@ -11,29 +11,28 @@ const daysOfWeek ={
     6:"Friday",
     7:"Saturday"
 }
-
-const today = daysOfWeek[1];
+const day = 5
 
 const LocationPage = () => {
     const [backGround, setBackground] = useState("#202020")
     const [temp, setTemp] = useState(55)
     const [condition, setCondition] = useState("Clear")
     const [location, setLoc] = useState("Smyrna")
-    const [DayOfWeek, setDay] = useState(today)
+    const [DayOfWeek, setDay] = useState(daysOfWeek[day])
     const [sun, setSun] = useState({"rise":"7:30","set":"7:00"})
     const [highsNlows, setHighsNLows] = useState({"high":65,"low":35})
-    const [rain, setRain] = useState(88)
-    const [humidity, setHumid] = useState(66)
+    const [rain, setRain] = useState(5)
+    const [humidity, setHumid] = useState(25)
     // const [hourlyForecast, setHourlyForecast] = useState({"high":65,"low":35})
     const [open, setOpen] = useState(true);
-    
     const prevScrollY = useRef(0);
     const [goingUp, setGoingUp] = useState(false);
+
     const [currentY, setY]= useState(0)
 
     useEffect(() => {
         console.log("Y changed", currentY)
-        if (currentY > 75 ){
+        if (currentY > 45 ){
             console.log("collapse", window.innerWidth)
             setOpen(false )
             
@@ -61,18 +60,19 @@ const LocationPage = () => {
         for(let i=0;i<hours;i++){
             forecast.push(
                 <ListGroup.Item className="bg-transparent  text-light text-center my-2" style={{"minWidth":'20%'}}>
-                    <p>{i == 0 ? "Now" : i}</p>
+                    <p>{i == 0 ? "Now" : `${i} PM`}</p>
                     <img style={{"width":"2em"}} src={TestIcon}/>
-                    <p>Temp</p>
+                    <p>{highsNlows.high}</p>
                 </ListGroup.Item>
             )
         }
         return forecast
     }
 
-    const tenDayForecast = (hours) =>{
+
+    const tenDayForecast = () =>{
         let forecast = []
-        for(let i=1;i<=hours;i++){
+        for(let i=day;i<=((10-day)+day) ;i++){
             forecast.push(
                 <ListGroup.Item className="justify-content-between d-flex flex-row bg-transparent  text-light " style={{}}>
                     <h5 className="w-25  text-start">{daysOfWeek[i>7 ? i-7 : i]} </h5>
@@ -95,25 +95,26 @@ const LocationPage = () => {
                
             
             </Navbar>
-            <Row className="justify-content-center  " >
-                <Col className="text-center bg-dark  m-1   "   sm={12} style={{zIndex:10, "position": "fixed"}} >
-                    <Row  className="justify-content-center "  >
+            <Row className="justify-content-center border" >
+                <Col className="text-center border  m-1"   sm={12} style={{zIndex:1, "position": "fixed", }} >
+                    <Row  className="justify-content-center" style= {{backgroundImage: "url('https://images.unsplash.com/photo-1514519273132-6a1abd48302c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format')",
+"backgroundRepeat": "no-repeat","backgroundSize": "100%"}}  >
                             <p className="mt-5 " >
                                 <span className="display-3 " > {location}</span> <br/>
                             <span className="" style={{}}>{condition}</span>
                             </p>
                             
-                            <p className="display-1" style={{"display":currentY<90?'':"none"}} >{temp} &deg; </p>
+                            <p className="display-1" style={{"display":currentY<45?'':"none"}} >{temp} &deg; </p>
                         
                         <Collapse in={open} dimension="height">
                                 <Row id="example-collapse-text"  className="bg-transparent m-2 justify-content-between"  style={{"height":'10%'}}>
-                                    <Col className=" bg-transparent"  xs={6}>
-                                        <p className="text-justify">{
-                                        DayOfWeek}  <span className='text-uppercase'>Today</span>
-                                        </p> 
+                                    <Col className=""  xs={6}>
+                                        <h5 className="text-start">
+                                            {DayOfWeek}  <span className='text-uppercase'>Today</span>
+                                        </h5> 
                                     </Col>
-                                    <Col  className="bg-transparent" xs={4}>
-                                        {highsNlows.high}    {highsNlows.low}
+                                    <Col  className="" xs={4}>
+                                       <h5>{highsNlows.high}    {highsNlows.low}</h5>
                                     </Col>
                                 </Row>
                         </Collapse>
@@ -141,22 +142,22 @@ const LocationPage = () => {
             
 
             <Row className="justify-content-between " style={{"height":'10%'}}>
-                <Col className=" bg-transparent my-2" xs={5} >
-                    Sunrise: {sun.rise}AM
+                <Col className="my-2" xs={5} >
+                    <h5>Sunrise: {sun.rise}AM</h5>
                 </Col>
 
-                <Col className="bg-transparent my-2 " xs={5} >
-                    Sunset: {sun.set}PM
+                <Col className=" my-2 " xs={5} >
+                    <h5>Sunset: {sun.set}PM</h5>
                 </Col>
             </Row>
 
             <Row className="justify-content-between  mb-5 " style={{"height":'10%'}}>
-                <Col className="bg-transparent my-2 "  xs={5} >
-                    Rain: {rain}%
+                <Col className=" my-2 "  xs={5} >
+                    <h5>Rain: {rain}%</h5>
                 </Col>
 
-                <Col className="bg-transparent my-2 " xs={5} >
-                    Humididty: {humidity}%
+                <Col className="my-2 " xs={5} >
+                    <h5>Humididty: {humidity}%</h5>
                 </Col>
             </Row>
             </Row>
